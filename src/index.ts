83d59514f3ba401ba6a133c3862844d9fdbdf919
ahdg <36436808+ahdg6@users.noreply.github.com>
2023-08-24 23:05:44 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,60 @@
-import { Context, Dict, Schema } from 'koishi'
-import { OpenAI } from 'openai'
+import { Context, Dict, Logger, Schema, trimSlash } from "koishi";
 
-export const name = 'why-not-ask-gpt'
+export const name = "why-not-ask-gpt";
 
 export interface Config {
-  apikey: string,
-  commands: Dict<string>,
+  apikey: string;
+  proxy: string;
+  area: string;
+  symbol: string;
+  commands: Dict<string>;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  apikey: Schema.string().required().description('OpenAI API Key (GPT-3.5-Turbo)'),
-  commands: Schema.dict(String).role('table')
-})
-
-async function askGPT(openai: OpenAI, info: string) {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: info }],
-    model: 'gpt-3.5-turbo',
-  })
-  const response = completion.choices
-  return response
-}
+  apikey: Schema.string()
+    .required()
+    .description("OpenAI API Key (GPT-3.5-Turbo)"),
+  proxy: Schema.string().default("https://api.openai.com"),
+  area: Schema.string()
+    .role("textarea", { rows: [2, 4] })
+    .default(
+      '你需要为我选择一个指令并仅回复整个指令，指令有 <> [] 标注的选项你应自主理解描述并填入选项，如果找不到相应指令的回复 "未找到"',
+    ),
+  symbol: Schema.string()
+    .default("未找到")
+    .description("找不到指令时约定提示词。"),
+  commands: Schema.dict(String).role("table"),
+});
 
 export function apply(ctx: Context, config: Config) {
-  const openai = new OpenAI({ apiKey: config.apikey })
-  let root = ctx.$commander
-  ctx.i18n.define('zh-CN', require('./zh'))
-  let cmds = ''
+  ctx.i18n.define("zh-CN", require("./zh"));
+
+  let cmds = "";
   for (const [key, value] of Object.entries(config.commands)) {
-    cmds += `${key} - ${value}\n`
+    cmds += `${key} - ${value}\n`;
   }
-  ctx.command("ask <info:text>")
-    .action(async ({ session }, info) => {
-      const ask = `我将在一个聊天程序的机器人提供的指令中选择一个指令来满足我的需求，这是这个机器人所提供的所有指令列表：
-                  ${cmds}
-                  你需要通过我接下来的描述为我选择一个指令并直接回复整个指令，不要有多余的话，有如果可以填入选项的填入选项
-                  ${info}`
-      session.send(ask)
-      const answer = await askGPT(openai, ask)
-      // session.send(answer[0].message.content)
-      session.execute(answer[0].message.content)
-  })
+
+  ctx.command("ask <info:text>").action(async ({ session }, info) => {
+    const response = await ctx.http.axios({
+      method: "post",
+      url: trimSlash(`${config.proxy}/v1/chat/completions`),
+      headers: {
+        Authorization: `Bearer ${config.apikey}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: config.area + "\n" + cmds }, // 换新行来保证指令识别
+          { role: "user", content: info },
+        ],
+      },
+      timeout: 600000,
+    });
+    const cmd: string = response.data["choices"][0]["message"]["content"];
+    if (cmd == config.symbol) {
+      return cmd;
+    }
+    session.execute(response.data["choices"][0]["message"]["content"]);
+  });
 }
